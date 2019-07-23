84217e2b61282eac4eefb6ef380160eb4e2a4460
Brooke Kullberg <brooke@>
2019-07-23 13:36:59 -0700

    fix reducer and test to pass

--- src/__tests__/reducers/category-reducer.test.js
+++ src/__tests__/reducers/category-reducer.test.js
@@ -1,8 +1,30 @@
-import categoryReducer from './../../src/reducers/category-reducer';
+import categoryReducer from './../../reducers/category-reducer';
 
 describe("categoryReducer", () => {
 
+    let action;
+    const sampleCategoryData = {
+        name: 'Job Search',
+        id: 0
+    }
+
     test('Should return default state if no action type is recognized', () => {
         expect(categoryReducer({}, { type: null})).toEqual({});
+    })
+
+    test('Should add new category data to categoryList', () => {
+        const { name, id } = sampleCategoryData;
+        action = {
+            type: 'ADD_CATEGORY',
+            name: name,
+            id: id
+        }
+        console.log(action)
+        expect(categoryReducer({}, action)).toEqual({
+            [id] : {
+                name: name,
+                id: id
+            }
+        })
     })
 })
--- src/reducers/category-reducer.js
+++ src/reducers/category-reducer.js
@@ -1,1 +1,17 @@
-export default 
+export default (state = {}, action) => {
+    console.log(action)
+    let newState;
+    const { name, id } = action
+    switch (action.type) {
+        case 'ADD_CATEGORY':
+            newState = Object.assign({}, state, {
+                [id]: {
+                    name: name,
+                    id: id
+                }
+            });
+            return newState;
+        default:
+            return state;
+    }
+};
